c3e4e6e405df8b09d8b858cad1c5a9c0545655c2
Arvind83291 <Arvind83291@gmail.com>
2024-05-09 18:55:55 +0530

    value-match.js file added

---
+++ value-match.js
@@ -0,0 +1,7 @@
+let value = [1,2,3,4,5];
+    let n = prompt("Type a number you want to match");
+	for(let i = 0; i < value; i++){
+		if(i == n){
+		  console.log("your value "+ n +" is matched on this index position "+i);
+		}
+	};	
