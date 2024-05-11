05359cf3c0e166d2edb0ec783b54605ffd4cd052
Arvind83291 <Arvind83291@gmail.com>
2024-05-11 17:00:44 +0530

    added the appropriate library to read the input from the user

--- value-match.js
+++ value-match.js
@@ -1,7 +1,20 @@
-let value = [1,2,3,4,5];
-    let n = prompt("Type a number you want to match");
-	for(let i = 0; i < value; i++){
-		if(i == n){
-		  console.log("your value "+ n +" is matched on this index position "+i);
-		}
-	};	
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Type a number you want to match: ', (input) => {
+    let value = [1, 2, 3, 4, 5];
+    let num = parseInt(input);
+
+    for (let i = 0; i < value.length; i++) {
+        if (value[i] === num) {
+            console.log(`Your value ${num} is matched at index position ${i}`);
+        }
+    }
+
+    rl.close();
+});
+
